dd04b9494150a790e5fcf1b922f0711f6d1a6728
Barral <igormagno.dev@gmail.com>
2026-01-07 12:10:47 -0300

    feat: :sparkles: Desafio: escrever código no Editor de Código em C# para exibir duas mensagens.

--- Escrever seu primeiro código C#/Program.cs
+++ Escrever seu primeiro código C#/Program.cs
@@ -1,4 +1,7 @@
 ﻿
+// Console.WriteLine("Olá, mundo.");
+
+//#############################################################################
 // Force
 // Build FAILED.
 // ##/Escrever seu primeiro código C#/Program.cs(2,1): error CS0103: The name 'console' does not exist in the current context [##/Escrever seu primeiro código C#/Escrever seu primeiro código C#.csproj]
@@ -7,10 +10,21 @@
 
 // console.WriteLine("Olá, mundo.");
 
-Console.WriteLine("Olá, mundo.");
+//#############################################################################
+// Console.Write("Congratulations!");
 
-Console.Write("Congratulations!");
+// Console.Write(" ");
 
-Console.Write(" ");
+// Console.Write("You wrote your first lines of code.");
 
-Console.Write("You wrote your first lines of code.");
+//#############################################################################
+// Desafio: escrever código no Editor de Código em C# para exibir duas mensagens
+
+// Escreva um código que produz a seguinte saída:
+
+// Output
+// This is the first line.
+// This is the second line.
+
+Console.WriteLine("This is the first line.");
+Console.WriteLine("This is the second line.");
