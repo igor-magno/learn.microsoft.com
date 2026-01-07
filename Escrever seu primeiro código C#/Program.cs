5bd6a5613f186ca82a6bb5cb2f07ace411ffd4f7
Barral <igormagno.dev@gmail.com>
2026-01-07 11:59:50 -0300

    feat: :sparkles: Forçando erro de build relacionado à sintaxe.

--- Escrever seu primeiro código C#/Program.cs
+++ Escrever seu primeiro código C#/Program.cs
@@ -1,2 +1,8 @@
 ﻿
-Console.WriteLine("Olá, mundo.");
+// Force
+// Build FAILED.
+// ##/Escrever seu primeiro código C#/Program.cs(2,1): error CS0103: The name 'console' does not exist in the current context [##/Escrever seu primeiro código C#/Escrever seu primeiro código C#.csproj]
+//      0 Warning(s)
+//      1 Error(s)
+
+// console.WriteLine("Olá, mundo.");
